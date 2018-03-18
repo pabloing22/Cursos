6b5dcc945df6dc25fb168003f940566d4a8d3ba5
pabloing22 <pabloing199364gmail.com>
2018-03-18 02:40:43 -0300

    holis

--- JavaScript/js/form1.js
+++ JavaScript/js/form1.js
@@ -1,10 +1,12 @@
 function sumar(a,b){
 
-  var a=document.getElementById('num1').value;
-  var b=document.getElementById('num2').value;
+  var a=parseInt(document.getElementById('num1').value);
+  var b=parseInt(document.getElementById('num2').value);
 
   var c=a+b;
 
-  return c;
+  //return c;
+
+  console.log(c);
 
 }
