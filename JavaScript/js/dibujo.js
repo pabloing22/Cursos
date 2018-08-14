59239b8404d4fd7e5589001129d35841891d6540
pabloing22 <pabloing1993@gmail.com>
2018-08-13 23:03:38 -0300

    last

---
+++ JavaScript/js/dibujo.js
@@ -0,0 +1,7 @@
+// var canvas=document.querySelector("#lienzo"); //otorgamos a la variable canvas un objeto que representa a nuestro canvas en HTML
+// var ctx=canvas.getContext("2d"); //obtenemos el contexto del objeto canvas;
+//
+// ctx.lineWidth=5;
+// ctx.strokeStyle="black";
+// ctx.rect=(0,0,999,499);
+// ctx.stroke();
